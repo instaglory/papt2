5f197cfb2f71c6236fa59c77c7309a426b2159e3
ghiffariarw <callmeghif@gmail.com>
2019-08-16 14:30:36 +0700

    bismillah

--- client/src/components/profile.vue
+++ client/src/components/profile.vue
@@ -22,7 +22,7 @@
    <div class="row no-gutters" style="margin-top: 10px">
       <div class="col-3 list-photo d-flex justify-content-center list-unstyled" v-for="photo in photos">
         <!-- <a href="#"> -->
-          <img v-bind:src="photo.img"/>
+          <img v-bind:src="photo.image"/>
         <!-- </a> -->
       </div>
     </div>
@@ -67,32 +67,7 @@ export default {
       message: 'Hello world',
       profileImg: null,
       bio: '',
-      photos: [
-        {
-          img: 'http://cameta.club/image_upload/tu/tumblr-bedroom-decor-fresh-teen-room-best-about-teenage_single-bedroom-701x451.jpg'
-        },
-        {
-          img: 'https://66.media.tumblr.com/ad33ef91750ea95d94a0e1dc611dc506/tumblr_p0cg3wUySw1woeuwro1_400.jpg'
-        },
-        {
-          img: 'https://i.pinimg.com/736x/68/22/a8/6822a8ec1cfbf78c54e5de9c0c938d38.jpg'
-        },
-        {
-          img: 'https://cfcdn.zulily.com/images/cache/product/452x1000/356055/zu71353474_main_tm1555077821.jpg'
-        },
-        {
-          img: 'http://www.ourartworld.com/photography/wp-content/uploads/2018/07/f8496c6e1d0af9c482483507a309d4b1--magic-umbrella-clear-umbrella.jpg'
-        },
-        {
-          img: 'https://images.unsplash.com/photo-1526116977494-90748acc0cad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-        },
-        {
-          img: 'https://images.unsplash.com/photo-1533003505519-6a9b92ed4911?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80'
-        },
-        {
-          img: 'https://images.unsplash.com/photo-1531279550271-23c2a77a765c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80'
-        }
-      ]
+      photos: []
     };
   },
   methods: {
@@ -100,16 +75,17 @@ export default {
       let formData = new FormData()
       formData.append('bio', this.bio)
       formData.append('profilePic', this.profileImg)
-      console.log(formData);
       axios({
           method: 'patch',
           url: `http://localhost:3000/users/update`,
           data: formData,
           headers:{
-              token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkNTYzMzgzMGU2NzljNjA2NWI3ZTZiYiIsImVtYWlsIjoiYXNkQG1haWwuY29tIiwidXNlcm5hbWUiOiJhbnhpZXR5IiwiaWF0IjoxNTY1OTM2NTU0fQ.2z9paLKfxVRMCj_f13LxiURqOb_LmAqSxsK-A61f_Jg'
+              token: localStorage.getItem('token')
           }
       })
       .then(response => {
+        console.log('object');
+        this.getProfile()
         Swal.fire(
         'Success',
         'profile updated!',
@@ -124,9 +100,41 @@ export default {
         )
       })
     },
+
     getProfileImage(){
       this.profileImg = event.target.files[0]
+    },
+
+    getProfile(){
+      console.log('masuk');
+      axios({
+        method: 'get',
+        url: 'http://localhost:3000/users'
+      })
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch(console.log(object))
+    },
+
+    getMyPhotos(){
+      axios({
+        method: 'get',
+        url: 'http://localhost:3000/posts/mypost',
+        headers: {
+          token: localStorage.getItem('token')
+        }
+      })
+      .then(({ data }) => {
+        console.log(data, 'data');
+        this.photos = data
+      })
+      .catch(console.log(err))
     }
+  },
+  created() {
+    
+    this.getMyPhotos()
   }
 };
 </script>
